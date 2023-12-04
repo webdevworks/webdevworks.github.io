7b763b98467e24f255ae0dd8329a896966659645
WebStudies <webstudies@outlook.com>
2023-12-05 00:57:29 +0100

    edit

--- src/components/NavbarCom.js
+++ src/components/NavbarCom.js
@@ -16,8 +16,8 @@ function ComNavbar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/about">Hakkimizda</Nav.Link>
-            <Nav.Link href="/contact">Iletisim</Nav.Link>
+            <Nav.Link href="#/about">Hakkimizda</Nav.Link>
+            <Nav.Link href="#/contact">Iletisim</Nav.Link>
             {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
